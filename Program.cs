7c9bee14dcb7939aa34f961f9ee2814f1504b579
dealer426 <burnssamuel01@gmail.com>
2020-01-22 20:50:52 -0500

    More Lists

--- Program.cs
+++ Program.cs
@@ -7,16 +7,20 @@ namespace sprint3lists
     {
         static void Main(string[] args)
         {
-            List<String> giantWords = new List<String>();
-            giantWords.Add("fee");
-            giantWords.Add("fie");
-            giantWords.Add("foe");
-            Console.WriteLine(giantWords);
-            Console.WriteLine("my list has " + giantWords.Count + " elements");
+            List<string> myTools = new List<string>();
+            myTools.Add("hammer");
+            myTools.Add("shovel");
+            myTools.Add("wrench");
 
-            giantWords.Add("fum");
-            Console.WriteLine(giantWords);
-            Console.WriteLine("my list has " + giantWords.Count + " elements");
+            if (myTools.Contains("hammer"))
+            {
+                Console.WriteLine("I have a hammer!");
+            }
+
+            foreach (string word in myTools)
+            {
+                Console.WriteLine(word + "! ");
+            }
         }
     }
 }
